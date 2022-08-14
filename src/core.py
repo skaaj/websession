dac0e1aee803f8a1b9d6c6b922f588ebc80a98c6
Benjamin Denom <ben.denom@gmail.com>
2022-08-14 11:22:39 +0200

    first implementation of 'create' subcommand

--- src/core.py
+++ src/core.py
@@ -1,3 +1,4 @@
+import os
 import json
 import glob
 from typing import Optional
@@ -5,12 +6,25 @@ from typing import Optional
 def load_session(name: str) -> dict:
     path = _find_session_path(name)
     raw_session = _load_json(path)
-    
     return raw_session
+
+def write_session(session: dict, scope: str) -> None:
+    name = session["name"]
+    obj = [{"url": it} for it in session["urls"]]
+    path = f"./data/{scope}/{name}.json"
+    _dump_json(obj, path)
+
+def session_exists(name: str, scope: str) -> bool:
+    return glob.glob(f"./data/user/{name}.json")
 
 def _load_json(path: str) -> dict:
     with open(path, "r") as f:
         return json.load(f)
+
+def _dump_json(obj: dict, path: str) -> None:
+    os.makedirs("/".join(path.split("/")[:-1]), exist_ok=True)
+    with open(path, "w+") as f:
+        json.dump(obj, f, indent=4)
 
 def _find_session_path(name: str) -> Optional[str]:
     filename = f"{name}.json"
--- src/create.py
+++ src/create.py
@@ -1,2 +1,35 @@
+from core import write_session, session_exists
+
 def create(args) -> None:
-    raise NotImplemented()
+    name = _get_session_name()
+    urls = _get_urls()
+    if urls:
+        _confirm_creation({
+            "name": name,
+            "urls": urls
+        })
+
+def _get_session_name() -> str:
+    name = input("Session name: ")
+    if session_exists(name, "user"):
+        raise RuntimeError(f"{name} session already exists.")
+    return name
+
+def _get_urls() -> list:
+    urls = []
+    while True:
+        url = input(f"URL {len(urls) + 1} (q to quit): ")
+        if url.lower() != "q":
+            urls.append(url)
+        else:
+            return urls
+
+def _confirm_creation(session: dict) -> None:
+    name = session["name"]
+    urls = session["urls"]
+    confirm = input(f"Confirm creation of {name} (size: {len(urls)}) ? (Y/n) ")
+    if confirm.lower() != "n":
+        write_session(session, "user")
+        print(f"New session 'user/{name}.json' created.")
+    else:
+        print("Creation aborted. Bye!")
